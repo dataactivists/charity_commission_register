d7ab89b7ad226292616883bc863742cb9a439d78
harabat <45575009+harabat@users.noreply.github.com>
2024-04-23 22:07:54 +0200

    add most frequent transferees analysis

--- code/charity_commission.py
+++ code/charity_commission.py
@@ -169,6 +169,42 @@ df['transferor'].value_counts()[:10]
 #
 # *The Parochial Church Council of the Ecclesiastical Parish of The A453 Churches of South Nottinghamshire* has managed to be the transferor 5 times. 
 
+# %% [markdown]
+# #### Most frequent transferors
 
 # %%
-df['transferor_number'].str.extract('(\d{5,})').value_counts()
+df['transferee_number'].value_counts()[:10]
+
+# %%
+df['transferee_number'].value_counts().iloc[:100].plot(kind='bar').set_xticks([])
+
+# %% [markdown]
+# Without counting the outlier that merged 1200+ times, some transferees have gone through mergers >40 times. 
+#
+# Most transferees only go through a merger <5 times.
+
+# %%
+df['transferee'].value_counts()[:10]
+
+# %%
+df[['transferor', 'transferee']].loc[
+    df['transferee'].str.contains('Kingdom Hall Trust')
+].head()
+
+# %%
+df[['transferor', 'transferee']].loc[
+    df['transferee'].str.contains('Victim Support')
+].head()
+
+# %%
+df[['transferor', 'transferee']].loc[
+    df['transferee'].str.contains('Mission to Seafarers')
+].head()
+
+# %% [markdown]
+# Summary from a [Brave](https://search.brave.com/search?q=The+Kingdom+Hall+Trust+&summary=1) search:
+#
+# The Kingdom Hall Trust:
+# - Previously known as the London Company of Kingdom Witnesses, it was established on 28th July 1939 and changed its name to The Kingdom Hall Trust on 20th June 1994.
+# - It is a charity associated with Jehovah’s Witnesses, with the charity number GB-CHC-275946.
+# - The charity has undergone a significant merger in 2022, incorporating 1,279 Jehovah’s Witness congregations into the national charity. This is considered one of the largest charity mergers ever.
