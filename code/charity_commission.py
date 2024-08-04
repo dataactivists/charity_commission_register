2ce29175929922b48e7bf91f806a2144b080962a
harabat <45575009+harabat@users.noreply.github.com>
2024-08-04 20:53:16 +0200

    update data quality indication

--- code/charity_commission.py
+++ code/charity_commission.py
@@ -55,9 +55,6 @@ import seaborn as sns
 # The data can be found at https://www.gov.uk/government/publications/register-of-merged-charities.
 
 # %% [markdown]
-# Caveats:
-#
-# - The merger registration data is not accurate, especially the timespans between the dates of transfer and registration can go from -1y to 32y, but varies wildly even when outliers are removed. Ideally, this will be raised with the Charity Commission.
 # - [Merging two or more Charitable Incorporated Organisations (CIOs)](https://www.gov.uk/government/publications/register-of-merged-charities/guidance-about-the-register-of-merged-charities#merging-two-or-more-charitable-incorporated-organisations-cios) does not require the merger to be registered. Consequently, the register of merged charities will be missing this data. Does this data need to be FOIA'd?
 
 # %% [markdown]
